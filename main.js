7e85e6a69022ce8885bd84a4f40dfa6e263296f6
Joan Gomez <joansebastiango@gmail.com>
2022-09-16 15:09:14 -0500

    menu-Desktop

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuDesktop = document.querySelector('.inactive');
+const navEmail = document.querySelector('.navbar-email');
+
+navEmail.addEventListener('click', () => {
+    menuDesktop.classList.toggle('inactive')
+});
+
+
