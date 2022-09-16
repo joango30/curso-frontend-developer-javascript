d1add1843ed7dfb9a23eede0dc47d1a819a56a2d
Joan Gomez <joansebastiango@gmail.com>
2022-09-16 18:55:10 -0500

    menuMObile

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+// menu Desktop
 const menuDesktop = document.querySelector('.inactive');
 const navEmail = document.querySelector('.navbar-email');
+
+const menuMobile = document.querySelector('.inactive-mobile');
+const himMenu = document.querySelector('.menu')
+
+
 
 navEmail.addEventListener('click', () => {
     menuDesktop.classList.toggle('inactive')
 });
+
+himMenu.addEventListener('click', () => {
+    menuMobile.classList.toggle('inactive-menu')
+});
+
 
 
